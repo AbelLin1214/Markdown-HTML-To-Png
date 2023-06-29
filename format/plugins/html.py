21a96fae4be33eca17d7e487ede626025f79b2ab
Abel <icyheart1214@163.com>
2023-06-29 09:17:49 +0800

    attempt to fix zombie process

--- format/plugins/html.py
+++ format/plugins/html.py
@@ -1,7 +1,7 @@
 '''
 Author: Abel
 Date: 2022-12-26 16:27:54
-LastEditTime: 2023-06-29 09:12:44
+LastEditTime: 2023-06-29 09:17:02
 '''
 import platform
 import time
@@ -9,25 +9,25 @@ from pathlib import Path
 from playwright.async_api import async_playwright
 
 async def html_to_png(html: str, selector: str=None):
-    async with async_playwright() as p:
-        b = await p.chromium.launch(
-            headless=True
-            )
-        c = await b.new_context(no_viewport=True)
-        p = await c.new_page()
-        try:
-            with TempUrl(content=html) as url:
-                await p.goto(url)
-                await p.wait_for_load_state('networkidle')  # sometimes need to load js via network
-                # await p.keyboard.press('Control+End')  # 滚至页面底部
-                selector = selector or '//body'
-                ele = await p.wait_for_selector(selector, state='visible')
-                img_bytes = await ele.screenshot(path='statics/temp/temp.png', scale='css')
-                return img_bytes
-        finally:
-            await p.close()
-            await c.close()
-            await b.close()
+    p = await async_playwright().start()
+    b = await p.chromium.launch(
+        headless=True
+        )
+    c = await b.new_context(no_viewport=True)
+    page = await c.new_page()
+    try:
+        with TempUrl(content=html) as url:
+            await page.goto(url)
+            await page.wait_for_load_state('networkidle')  # sometimes need to load js via network
+            # await page.keyboard.press('Control+End')  # 滚至页面底部
+            selector = selector or '//body'
+            ele = await page.wait_for_selector(selector, state='visible')
+            img_bytes = await ele.screenshot(path='statics/temp/temp.png', scale='css')
+            return img_bytes
+    finally:
+        await c.close()
+        await b.close()
+        await p.stop()
 
 class TempUrl:
     '''临时文件处理器'''
