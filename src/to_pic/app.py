99dc3e5a32a80240d51f02ab20a45bd6ede5aa9a
Abel <icyheart1214@163.com>
2023-12-27 10:35:47 +0800

    🎈 perf: 完善swagger

--- src/to_pic/app.py
+++ src/to_pic/app.py
@@ -1,13 +1,14 @@
 '''
 Author: Abel
 Date: 2022-12-26 17:18:18
-LastEditTime: 2023-12-27 10:03:33
+LastEditTime: 2023-12-27 10:35:32
 '''
 from pathlib import Path
 from loguru import logger
 from fastapi import FastAPI
 from fastapi.responses import Response
 from .format import transfer
+from typing import Literal
 from pydantic import BaseModel, Field
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -28,17 +29,17 @@ def png_response(img_bytes: bytes):
     )
 
 class Item(BaseModel):
-    type_: str = Field(..., alias='type')
-    content: str = None
-    selector: str = None
-    proxy: str = None
-    url: str = None
+    type_: Literal['markdown', 'html'] = Field(..., alias='type', examples=['html'])
+    content: str = Field(None, examples=['<h1>hello world</h1>'])
+    selector: str = Field(None, examples=['//h1'])
+    proxy: str = Field(None, examples=['http://localhost:8888'])
+    url: str = Field(None, examples=['https://www.baidu.com'])
 
-@app.get('/temp/{file_name}')
+@app.get('/temp/{file_name}', include_in_schema=False)
 async def to_temp_dir(file_name: str):
     return FileResponse(f'./temp/{file_name}')
 
-@app.get('/favicon.ico')
+@app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
     return FileResponse('./statics/favicon.ico')
 
@@ -50,7 +51,7 @@ async def to_png(item: Item):
     if item.content is None and item.url is None:
         return {'code': -1, 'msg': '请求错误: url和content不能同时为空'}
     img_bytes = await transfer(
-        **item.dict()
+        **item.model_dump()
         )
     if not img_bytes:
         return {'code': -1, 'msg': '请求错误：不支持当前格式'}
